b2001b7d9e05c540b0b70ba3d3326cf181aa75a1
Duy Pham <87891876+Bibententen@users.noreply.github.com>
2026-08-24 13:03:59 +1000

    chore: add tests/routes.ts

---
+++ tests/routes.ts
@@ -0,0 +1,12 @@
+export const routes = [
+  "/",
+  "/about",
+  "/projects",
+  "/projects/melbourne-rental-prediction",
+  "/projects/customer-clustering",
+  "/projects/prostate-cancer-classification",
+  "/projects/tweet-text-pipeline",
+  "/projects/coffee-sales-excel",
+  "/contact",
+  "/resume",
+] as const;
